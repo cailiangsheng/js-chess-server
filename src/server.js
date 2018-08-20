42bdd2578b3a17aec0cdaf9b9a42087b36e93979
Larry Cai <larry.cai@sigma.name>
2018-08-20 13:25:29 +0800

    disconnect event

--- src/server.js
+++ src/server.js
@@ -1,23 +1,38 @@
 import express from 'express'
 import { createServer } from 'http'
 import io from 'socket.io'
+import _ from 'lodash'
 
 const app = express()
 const server = createServer(app)
 const socket = io(server)
 
+let clientSockets = null
+
 const startServer = (port) => {
+	if (clientSockets) return clientSockets
+
+	clientSockets = []
+
 	socket.on('connection', (clientSocket) => {
-		console.log('connected a socket')
-	
+		console.log('Connected a socket')
+		clientSockets.push(clientSocket)
+
+		clientSocket.on('disconnect', (reason) => {
+			console.log('Disconnected a socket')
+			_.pull(clientSockets, clientSocket)
+		})
+
 		clientSocket.on('hello', (data) => {
 			console.log('hello', data)
 		})
 	})
 
 	server.listen(port, () => {
-		console.log('server listening on', port, '...')
+		console.log('Server listening on', port, '...')
 	})
+
+	return clientSockets
 }
 
 export {
